1d517d433b74dea54ec7b3b42395d9ccf82cc753
Alexandre Jablon <alexandre.jablon@gmail.com>
2018-12-02 19:47:57 +0100

    Add more icon examples

--- stories/icon.js
+++ stories/icon.js
@@ -1,10 +1,30 @@
 import React from 'react'
+import feather from 'feather-icons'
 
 import Icon from '../src/components/Icon'
 
 export default [
   {
     name: 'Icon',
-    element: <Icon name="feather" />,
+    element: Object.keys(feather.icons).map(name => (
+      <Icon name={name} title={name} />
+    )),
+  },
+  {
+    name: 'Icon sizes',
+    element: (
+      <>
+        <Icon name="layers" title="12px" size="12px" />
+        <Icon name="layers" title="14px" size="14px" />
+        <Icon name="layers" title="16px" size="16px" />
+        <Icon name="layers" title="18px" size="18px" />
+        <Icon name="layers" title="20px" size="20px" />
+        <Icon name="layers" title="24px" size="24px" />
+        <Icon name="layers" title="28px" size="28px" />
+        <Icon name="layers" title="32px" size="32px" />
+        <Icon name="layers" title="40px" size="40px" />
+        <Icon name="layers" title="50px" size="50px" />
+      </>
+    ),
   },
 ]
