9487112cc059a9cecc05765ed3d775172a42e01a
Alexandre Jablon <alexandre.jablon@gmail.com>
2018-12-30 12:37:41 +0100

    Add headings stories, update snapshots

---
+++ stories/headings.stories.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import feather from 'feather-icons'
+
+import { H1, H2, H3, H4, H5, H6 } from '../src/components/Heading'
+
+export const description = 'Headings'
+
+export default [
+  {
+    name: 'Headings',
+    element: (
+      <>
+        <H1>
+          Voix ambiguë d'un cœur qui au zéphir préfère les jattes de kiwi.
+        </H1>
+        <H2>
+          Voix ambiguë d'un cœur qui au zéphir préfère les jattes de kiwi.
+        </H2>
+        <H3>
+          Voix ambiguë d'un cœur qui au zéphir préfère les jattes de kiwi.
+        </H3>
+        <H4>
+          Voix ambiguë d'un cœur qui au zéphir préfère les jattes de kiwi.
+        </H4>
+        <H5>
+          Voix ambiguë d'un cœur qui au zéphir préfère les jattes de kiwi.
+        </H5>
+        <H6>
+          Voix ambiguë d'un cœur qui au zéphir préfère les jattes de kiwi.
+        </H6>
+      </>
+    ),
+  },
+]
