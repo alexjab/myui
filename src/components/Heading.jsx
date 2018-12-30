717cb0e0f409d52978b414fd96a9c90ff6c0962d
Alexandre Jablon <alexandre.jablon@gmail.com>
2018-12-30 12:38:00 +0100

    Use font-face, add Inter UI fonts

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,33 +1,44 @@
 import React from 'react'
 import styled from 'styled-components'
 
-export const H1 = styled.h1`
-  margin-top: 0;
-  margin-bottom: 0;
-  ${({ theme }) => theme.colorGreyDarker}
+import { light as lightTheme } from '../core/themes'
+
+export const H1 = styled.div`
+  font-size: 2.5em;
+  font-weight: 600;
+  color: ${({ theme }) => theme.colorDarkest};
 `
 
-export const H2 = styled.h2`
-  margin-top: 0;
-  margin-bottom: 0;
+H1.defaultProps = {
+  theme: lightTheme,
+}
+
+export const H2 = styled.div`
+  font-size: 2em;
+  font-weight: 600;
+  color: ${({ theme }) => theme.colorDarkest};
 `
 
-export const H3 = styled.h3`
-  margin-top: 0;
-  margin-bottom: 0;
+export const H3 = styled.div`
+  font-size: 1.75em;
+  font-weight: 600;
+  color: ${({ theme }) => theme.colorDarkest};
 `
 
-export const H4 = styled.h4`
-  margin-top: 0;
-  margin-bottom: 0;
+export const H4 = styled.div`
+  font-size: 1.5em;
+  font-weight: 600;
+  color: ${({ theme }) => theme.colorDarkest};
 `
 
-export const H5 = styled.h5`
-  margin-top: 0;
-  margin-bottom: 0;
+export const H5 = styled.div`
+  font-size: 1.25em;
+  font-weight: 600;
+  color: ${({ theme }) => theme.colorDarkest};
 `
 
-export const H6 = styled.h6`
-  margin-top: 0;
-  margin-bottom: 0;
+export const H6 = styled.div`
+  font-size: 1.125em;
+  font-weight: 600;
+  color: ${({ theme }) => theme.colorDarkest};
 `
