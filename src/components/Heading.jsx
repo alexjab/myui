97d63d0c66efaa68e1ef51ca702672a6c8960545
Alexandre Jablon <alexandre.jablon@gmail.com>
2018-12-31 10:16:13 +0100

    Improvements to Headings:  - use h1, h2, h3, ... tags as base,  - add lightTheme in defaultProps

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -3,7 +3,9 @@ import styled from 'styled-components'
 
 import { light as lightTheme } from '../core/themes'
 
-export const H1 = styled.div`
+export const H1 = styled.h1`
+  margin-top: 0;
+  margin-bottom: 0;
   font-size: 2.5em;
   font-weight: 600;
   color: ${({ theme }) => theme.colorDarkest};
@@ -13,32 +15,62 @@ H1.defaultProps = {
   theme: lightTheme,
 }
 
-export const H2 = styled.div`
+export const H2 = styled.h2`
+  margin-top: 0;
+  margin-bottom: 0;
   font-size: 2em;
   font-weight: 600;
   color: ${({ theme }) => theme.colorDarkest};
 `
 
-export const H3 = styled.div`
+H2.defaultProps = {
+  theme: lightTheme,
+}
+
+export const H3 = styled.h3`
+  margin-top: 0;
+  margin-bottom: 0;
   font-size: 1.75em;
   font-weight: 600;
   color: ${({ theme }) => theme.colorDarkest};
 `
 
-export const H4 = styled.div`
+H3.defaultProps = {
+  theme: lightTheme,
+}
+
+export const H4 = styled.h4`
+  margin-top: 0;
+  margin-bottom: 0;
   font-size: 1.5em;
   font-weight: 600;
   color: ${({ theme }) => theme.colorDarkest};
 `
 
-export const H5 = styled.div`
+H4.defaultProps = {
+  theme: lightTheme,
+}
+
+export const H5 = styled.h5`
+  margin-top: 0;
+  margin-bottom: 0;
   font-size: 1.25em;
   font-weight: 600;
   color: ${({ theme }) => theme.colorDarkest};
 `
 
-export const H6 = styled.div`
+H5.defaultProps = {
+  theme: lightTheme,
+}
+
+export const H6 = styled.h6`
+  margin-top: 0;
+  margin-bottom: 0;
   font-size: 1.125em;
   font-weight: 600;
   color: ${({ theme }) => theme.colorDarkest};
 `
+
+H6.defaultProps = {
+  theme: lightTheme,
+}
