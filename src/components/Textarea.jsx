4f75557e09f09a3e307fe6949145737dc205e04a
Alexandre Jablon <alexandre.jablon@gmail.com>
2019-01-20 21:56:03 +0100

    Add Textarea component

---
+++ src/components/Textarea.jsx
@@ -0,0 +1,170 @@
+import React from 'react'
+import styled from 'styled-components'
+import { darken, transparentize } from 'polished'
+
+import { light as lightTheme } from '../core/themes'
+
+const StyledTextarea = styled.textarea`
+  font-family: ${props => props.theme.fontFamily};
+  font-size: ${({ isLarge, theme }) =>
+    isLarge ? theme.largeFontSize : theme.fontSize};
+
+  padding-left: ${({ isLarge }) => (isLarge ? '12px' : '10px')};
+  padding-right: ${({ isLarge }) => (isLarge ? '12px' : '10px')};
+  padding-top: ${({ isLarge }) => (isLarge ? '10px' : '6px')};
+  padding-bottom: ${({ isLarge }) => (isLarge ? '10px' : '6px')};
+
+  border: 1px solid
+    ${({ isFocused, theme, value }) => {
+      if (isFocused) {
+        return theme.colorPrimary
+      }
+
+      if (value) {
+        return theme.colorGreyDark
+      }
+
+      return 'transparent'
+    }};
+  box-shadow: ${({ isFocused, theme }) => {
+    if (isFocused) {
+      return `0 0 10px ${transparentize(0.3, theme.colorPrimary)}`
+    }
+
+    return 'none'
+  }};
+  background-color: ${({ isFocused, theme, value }) => {
+    if (isFocused || value) {
+      return 'transparent'
+    }
+
+    return darken(0.03, theme.colorGreyLightest)
+  }};
+  border-radius: 4px;
+
+  resize: none;
+`
+
+// const StyledInput = styled.input`
+//   font-family: ${props => props.theme.fontFamily};
+//   font-size: ${({ isLarge, theme }) =>
+//     isLarge ? theme.largeFontSize : theme.fontSize};
+
+//   position: relative;
+//   width: 100%;
+//   border: none;
+//   outline: none;
+//   background: none;
+
+//   height: ${({ isLarge }) => {
+//     return isLarge ? '36px' : '28px'
+//   }};
+
+//   margin-left: ${({ iconLeft, isLarge }) => {
+//     if (isLarge) {
+//       return iconLeft ? 0 : '10px'
+//     }
+//     return iconLeft ? 0 : '8px'
+//   }};
+//   margin-right: ${({ iconRight, isLarge }) => {
+//     if (isLarge) {
+//       return iconRight ? 0 : '10px'
+//     }
+//     return iconRight ? 0 : '8px'
+//   }};
+// `
+
+class Textarea extends React.Component {
+  static defaultProps = {
+    theme: lightTheme,
+  }
+
+  state = {
+    value: '',
+  }
+
+  handleChange = event => {
+    const value = event.target.value
+    const { value: valueProps, onChange, onChangeValue } = this.props
+
+    if (valueProps !== undefined) {
+      if (onChange) {
+        event.persist()
+        onChange(event)
+      }
+
+      if (onChangeValue) {
+        onChangeValue(value)
+      }
+      return
+    }
+
+    this.setState({ value }, () => {
+      if (onChange) {
+        event.persist()
+        onChange(event)
+      }
+
+      if (onChangeValue) {
+        onChangeValue(value)
+      }
+    })
+  }
+
+  handleFocus = event => {
+    const { onFocus } = this.props
+
+    this.setState({ isFocused: true }, () => {
+      if (onFocus) {
+        event.persist()
+        onFocus(event)
+      }
+    })
+  }
+
+  handleBlur = () => {
+    const { onBlur } = this.props
+
+    this.setState({ isFocused: false }, () => {
+      if (onBlur) {
+        event.persist()
+        onBlur(event)
+      }
+    })
+  }
+
+  setInputRef = ref => {
+    this.inputElement = ref
+  }
+
+  focus = () => {
+    this.inputElement.focus()
+  }
+
+  handleMouseDown = event => {
+    event.preventDefault()
+
+    this.focus()
+  }
+
+  render() {
+    const { isFullwidth, onChangeValue, theme, ...props } = this.props
+    const { isFocused } = this.state
+    const value = this.state.value || this.props.value || ''
+
+    return (
+      <StyledTextarea
+        theme={theme}
+        value={value}
+        isFocused={isFocused}
+        isFullwidth={isFullwidth}
+        onFocus={this.handleFocus}
+        onBlur={this.handleBlur}
+        onChange={this.handleChange}
+        {...props}
+      />
+    )
+  }
+}
+
+export default Textarea
---
+++ stories/textarea.stories.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import Textarea from '../src/components/Textarea'
+
+export const description = 'Textarea'
+
+export default [
+  {
+    name: 'Default',
+    element: <Textarea placeholder="This is a textarea" rows={7} cols={50} />,
+  },
+]
